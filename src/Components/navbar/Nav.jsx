006597f0eeb9260afa0a81f0186b9ab144ffd03c
Lts-star <onezera2@gmail.com>
2022-05-29 22:57:46 +0100

    react hook

--- src/Components/navbar/Nav.jsx
+++ src/Components/navbar/Nav.jsx
@@ -1,29 +1,52 @@
-import React from 'react'
-import './nav.css'
-import {GiHumanTarget} from 'react-icons/gi'
-import {BiHomeAlt} from 'react-icons/bi'
-import {RiContactsBookLine} from 'react-icons/ri'
-import {MdMiscellaneousServices} from 'react-icons/md'
-import {VscVmRunning} from 'react-icons/vsc'
-import { IconContext } from 'react-icons'
-import {useState} from 'react'
+import React from "react";
+import "./nav.css";
+import { GiHumanTarget } from "react-icons/gi";
+import { BiHomeAlt } from "react-icons/bi";
+import { RiContactsBookLine } from "react-icons/ri";
+import { MdMiscellaneousServices } from "react-icons/md";
+import { VscVmRunning } from "react-icons/vsc";
+import { IconContext } from "react-icons";
+import { useState } from "react";
 
-
-function nav() {
-  const[activeNav, setActiveNav ] =useState('#')
+function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <IconContext.Provider
-    value={{size:'30px'}}
-    >
-  <div className='navbar'>
-    <a href='#' className={activeNav === '#' ? 'active':''}><BiHomeAlt /></a>
-    <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><GiHumanTarget/></a> 
-    <a href='#experience' ><VscVmRunning/></a>
-    <a href='#services' ><MdMiscellaneousServices/></a>
-    <a href='#contact' ><RiContactsBookLine/></a>
-
-  </div></IconContext.Provider>
-  )
+    <IconContext.Provider value={{ size: "30px" }}>
+      <nav>
+        <a href="#" className={activeNav === "#" ? "active" : ""}>
+          <BiHomeAlt />
+        </a>
+        <a
+          href="#about"
+          onClick={() => setActiveNav("#about")}
+          className={activeNav === "#about" ? "active" : ""}
+        >
+          <GiHumanTarget />
+        </a>
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
+          <VscVmRunning />
+        </a>
+        <a
+          href="#services"
+          onClick={() => setActiveNav("#services")}
+          className={activeNav === "#services" ? "active" : ""}
+        >
+          <MdMiscellaneousServices />
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("#contact")}
+          className={activeNav === "#contact" ? "active" : ""}
+        >
+          <RiContactsBookLine />
+        </a>
+        </nav>
+    </IconContext.Provider>
+  );
 }
 
-export default nav
+export default Nav;
