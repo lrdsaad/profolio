9b3d941b3ad90a08d54e34625fb9054f337b8db3
Lts-star <onezera2@gmail.com>
2022-06-04 14:30:09 +0100

    about and experience(without css)

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me.png";
+import { FaRunning } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
-function about() {
+function About() {
   return (
     <section id="about">
-      
+      <h5>Get to know </h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me_image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        
+        <div className="about__content">
+        <div className="about__cards">
+        <article className="about__card">
+          <FaRunning className="about__icon" />
+          <h5>Experience</h5>
+          <small>3+ Years Working</small>
+        </article>
+        
+        <article className="about__card">
+          <BsPeopleFill className="about__icon" />
+          <h5>Clients</h5>
+          <small>300+ World wide clients</small>
+        </article>
+
+        <article className="about__card">
+          <AiOutlineFundProjectionScreen className="about__icon" />
+          <h5>Projects</h5>
+          <small>3+ Completed</small>
+        </article>
+
+        </div>
+        
+         <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          officia vel libero minus, itaque culpa? Rerum, iste. Numquam facilis
+          sed deserunt adipisci, voluptatem enim pariatur dolore reiciendis
+          commodi nihil sunt!
+        </p>
+        <a href="#contact" className="btn btn-primary">
+          Let's Talk
+        </a>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default about
+export default About;
--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 function experience() {
   return (
-      <section id="experience">
+    <section id="experience">
+      <h5>What Skills I Have </h5>
+      <h2>My Experience</h2>
 
-      </section>
-    )
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>Html</h4>
+              <small className="text-light">intermidiate</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>css</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>javaScript</h4>
+              <small className="text-light">intermidiate</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>React</h4>
+              <small className="text-light">experienced</small>
+            </article>
+          </div>
+        {/* End of Frontend */}
+        </div>
+        <div className="experince__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>Node</h4>
+              <small className="text-light">experiences</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>postman</h4>
+              <small className="text-light">beginner</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>MongoDB</h4>
+              <small className="text-light">beginner</small>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <h4>PHP</h4>
+              <small className="text-light">beginner</small>
+            </article>
+        </div>
+        {/* End of Backend */}
+
+      </div>
+      </div>
+
+    </section>
+  );
 }
 
-export default experience
+export default experience;
