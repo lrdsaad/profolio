4a55c7d7857fd23983ebb649a17c44b0f66d1e04
Lts-star <onezera2@gmail.com>
2022-06-04 15:26:42 +0100

    completed experience css

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -14,23 +14,29 @@ function experience() {
           <div className="experience__content">
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
-              <h4>Html</h4>
-              <small className="text-light">intermidiate</small>
+             <div> <h4>Html</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
-              <h4>css</h4>
-              <small className="text-light">experienced</small>
+              <div>
+              <h4>Css</h4>
+              <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
-              <h4>javaScript</h4>
-              <small className="text-light">intermidiate</small>
+              <div>
+              <h4>JavaScript</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
-              <h4>React</h4>
-              <small className="text-light">experienced</small>
+            <div>  <h4>React</h4>
+              <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
         {/* End of Frontend */}
@@ -40,28 +46,104 @@ function experience() {
           <div className="experience__content">
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
+              <div>
               <h4>Node</h4>
-              <small className="text-light">experiences</small>
+              <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
+              <div>
               <h4>postman</h4>
-              <small className="text-light">beginner</small>
+              <small className="text-light">Basics</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
+              <div>
               <h4>MongoDB</h4>
-              <small className="text-light">beginner</small>
+              <small className="text-light">Basics</small>
+              </div>
             </article>
             <article className="experience__details">
               <IoMdCheckmarkCircleOutline />
+              <div>
               <h4>PHP</h4>
-              <small className="text-light">beginner</small>
+              <small className="text-light">Basics</small>
+              </div>
             </article>
         </div>
         {/* End of Backend */}
 
       </div>
+      <div className="experience__mobile">
+          <h3>Mobile Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+             <div>
+                <h4>Flutter</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <div>
+              <h4>Css</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <div>
+              <h4>JavaScript</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+            <div> 
+               <h4>React</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        {/* End of Mobile developement */}
+        </div>
+        <div className="experience__AI">
+          <h3>Artificial Inteligence</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+             <div>
+                <h4>JavaScript</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <div>
+              <h4>Julia</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+              <div>
+              <h4>Lisp</h4>
+              <small className="text-light">Intermidiate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoMdCheckmarkCircleOutline />
+            <div> 
+               <h4>Python</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        {/* End of automating systems and scraping */}
+        </div>
       </div>
 
     </section>
